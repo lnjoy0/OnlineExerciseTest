962e4e2ef38213b4ebf6ca168d251380fbb2dc11
Enjoy-git <enjoy_july@163.com>
2020-06-20 13:34:26 +0800

    ready

--- testsystem/views.py
+++ testsystem/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Paper
 
 # Create your views here.
 def index(request):
+    paper_list = Paper.objects.order_by('-id')
+    
+
+
+def paper(request):
+    pass
+
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def logout(request):
+    pass
+
+def hash_code(s,salt='login'):
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())
+    return h.hexdigest()
+
+def cal_score(request):
     pass
